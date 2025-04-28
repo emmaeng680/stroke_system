ccc6030d339773aaad7cd8174876f0e2fe7e0a33
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2025-04-28 14:23:51 -0500

    Update settings.py

--- stroke_system/settings.py
+++ stroke_system/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '.vercel.app', '.now.sh', '127.0.0.1', 'stroke-system-2.onrender.com']
+ALLOWED_HOSTS = ['localhost', '.vercel.app', '.now.sh', '127.0.0.1', 'stroke-system-4.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
